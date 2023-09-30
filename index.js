8d9addc0ed1021a4c4bbbc94d297c286d0df1c6a
tatyanadon” “ <tatyanadon1@gmail.com>
2023-09-30 11:41:38 -0500

    implement CRUD for users

--- index.js
+++ index.js
@@ -1,30 +1,173 @@
-let users = [];
+let GLOBAL_USERS = [];
+let EDITING_USER = null;
 
-fetch("http://127.0.0.1:8000/users", {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-  },
-})
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data);
-    if (Array.isArray(data)) {
-      renderUsers(data)
-    }
+// JSDoc -> JS Documentating
+/**
+ * @type {HTMLFormElement}
+ */
+const userForm = document.getElementById("createUserForm");
+
+/**
+ * @type {HTMLInputElement}
+ */
+const userFormFirstNameField = document.getElementById('firstName');
+
+/**
+ * @type {HTMLInputElement}
+ */
+const userFormLastNameField = document.getElementById('lastName');
+
+/**
+ * @type {HTMLInputElement}
+ */
+const userFormAgeField = document.getElementById('age');
+
+
+/**
+ * @type {HTMLButtonElement}
+ */
+const formSubmitBtn = document.getElementById("createUserFormSubmit");
+
+/**
+ * @type {HTMLSpanElement}
+ */
+const formNotif = document.getElementById("createUserFormNotification");
+
+function createUser(user) {
+  return fetch("http://127.0.0.1:8000/user", {
+    method: "POST",
+    headers: {
+      accept: "application/json",
+      ["content-type"]: "application/json; charset=utf-8",
+    },
+    body: JSON.stringify(user),
+  }).then((res) => {
+    if (res.status !== 200) throw new Error("Failed to create user");
+    return res.json();
   });
+}
+
+function deleteUser(user) {
+  const url = new URL("http://127.0.0.1:8000/user");
+  url.searchParams.set("id", user.id);
+
+  // "http://127.0.0.1:8000/user?" + params.toString()
+  return fetch(url.toString(), {
+    method: "DELETE",
+    headers: {
+      accept: "application/json",
+    },
+  }).then((res) => {
+    if (res.status !== 200) throw new Error("Failed to create user");
+    return res;
+  });
+}
+
+function updateUser(user) {
+  debugger;
+  return fetch("http://127.0.0.1:8000/user", {
+    method: "PUT",
+    headers: {
+      accept: "application/json",
+      ["content-type"]: "application/json; charset=utf-8",
+    },
+    body: JSON.stringify(user),
+  }).then((res) => {
+    if (res.status !== 200) throw new Error("Failed to create user");
+    return res.json();
+  });
+  // .then((userWithId) => {
+  //   GLOBAL_USERS = GLOBAL_USERS.map(u => u.id === user.id ? user : u);
+  //   renderUsers(GLOBAL_USERS);
+  //   return userWithId
+  // });
+}
+
+function fetchUsers() {
+  return fetch("http://127.0.0.1:8000/users", {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      return data;
+    });
+}
+
+
+function switchFormToCreateUser() {
+  /**
+   * @type {HTMLButtonElement}
+   */
+  const formCancelBtn = document.getElementById('createUserFormCancel')
+  formCancelBtn.remove();
+  formSubmitBtn.innerText = 'Create User';
+}
+
+function switchFormToEditUser() {
+  /**
+   * @type {HTMLButtonElement}
+   */
+  const formCancelBtn = document.createElement("button");
+  formCancelBtn.id = "createUserFormCancel";
+  formCancelBtn.type = "reset";
+  formCancelBtn.innerText = "cancel editing";
+  formCancelBtn.addEventListener("click", (e) => {
+    // no prevent dafault to reset form
+    switchFormToCreateUser();
+    userForm.reset()
+  });
+  userForm.insertBefore(formCancelBtn, formNotif);
+  formSubmitBtn.innerText = 'Update User'
+
+  userFormFirstNameField.value = EDITING_USER.firstName;
+  userFormLastNameField.value = EDITING_USER.lastName;
+  userFormAgeField.value = EDITING_USER.age;
+}
 
 const renderUsers = (users) => {
   /**
    * @type {HTMLDivElement}
    */
-  const container = document.getElementById('userList');
-  const list = document.createElement('ul');
+  const container = document.getElementById("userList");
+  const list = document.createElement("ul");
   users.forEach((u) => {
-    const listItem = document.createElement('li');
+    const listItem = document.createElement("li");
+    // listItem.classList.add('my-list-item')
     listItem.innerText = `FirstName: ${u.firstName}, LastName: ${u.lastName}, age: ${u.age}`;
+    /**
+     * @type {HTMLButtonElement}
+     */
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "delete";
+    deleteBtn.addEventListener("click", (e) => {
+      deleteBtn.disabled = true;
+      deleteUser(u)
+        .then(() => {
+          GLOBAL_USERS = GLOBAL_USERS.filter((g) => g.id !== u.id);
+          renderUsers(GLOBAL_USERS);
+        })
+        .finally(() => {
+          deleteBtn.disabled = false;
+        });
+    });
+    listItem.appendChild(deleteBtn);
+    /**
+     * @type {HTMLButtonElement}
+     */
+    const editBtn = document.createElement("button");
+    editBtn.innerText = "edit";
+    editBtn.addEventListener("click", (e) => {
+      debugger;
+      EDITING_USER = u;
+      switchFormToEditUser();
+    });
+    listItem.appendChild(editBtn);
     list.appendChild(listItem);
-  })
+  });
   container.replaceChildren(list);
   // NOTE: alternative
   // while(container.firstChild)
@@ -32,75 +175,75 @@ const renderUsers = (users) => {
   //   container.removeChild(container.firstChild);
   // }
   // container.appendChild(newChild)
-}
-
-// JSDoc -> JS Documentating
-/**
- * @type {HTMLFormElement}
- */
-const userForm = document.getElementById('createUserForm');
-
-/**
- * @type {HTMLButtonElement}
- */
-const btn = document.getElementById('createUserFormSubmit');
-
-/**
- * @type {HTMLSpanElement}
- */
-const formNotif = document.getElementById('createUserFormNotification');
+};
 
 let prevNotificationTimeout;
 
-userForm.addEventListener('submit', (e) => {
+userForm.addEventListener("submit", (e) => {
   // form will submit post request to current url or `action` url by default
   e.preventDefault();
 
-  btn.disabled = true
+  formSubmitBtn.disabled = true;
   clearTimeout(prevNotificationTimeout);
-  formNotif.innerText = ''
+  formNotif.innerText = "";
 
   /**
-  * @type {HTMLFormElement}
-  */
+   * @type {HTMLFormElement}
+   */
   const form = e.target;
   const formData = new FormData(form);
   const user = {
-    firstName: formData.get('firstName'),
-    lastName: formData.get('lastName'),
-    age: parseInt(formData.get('age')),
+    firstName: formData.get("firstName"),
+    lastName: formData.get("lastName"),
+    age: parseInt(formData.get("age")),
+  };
+  console.log("user", user);
+
+  if (!EDITING_USER) {
+    return createUser(user)
+      .then((userWithId) => {
+        formNotif.innerText = "Success";
+        GLOBAL_USERS.push(userWithId);
+        renderUsers(GLOBAL_USERS);
+      })
+      .catch(() => {
+        formNotif.innerText = "Failed to create user";
+      })
+      .finally(() => {
+        formSubmitBtn.disabled = false;
+        prevNotificationTimeout = setTimeout(() => {
+          formNotif.innerText = "";
+        }, 3000);
+      });
   }
-  console.log('user', user)
 
-  fetch("http://127.0.0.1:8000/user", {
-    method: "POST",
-    headers: {
-      accept: "application/json",
-      ['content-type']: 'application/json; charset=utf-8',
-    },
-    body: JSON.stringify(user),
-  }).then((res) => {
-    if (res.status !== 200) throw new Error('Failed to create user');
-    formNotif.innerText = 'Success';
-    users.push(user);
-    renderUsers(users)
-
-    // if (res.status === 200) {
-    //   console.log("user created");
-    //   formNotif.innerText = 'Success';
-    //   return
-    // }
-    // throw new Error('Failed to create user');
+  updateUser({
+    ...user,
+    id: EDITING_USER.id,
   })
-  .catch(() => {
-    formNotif.innerText = 'Failed to create user';
-  })
-  .finally(() => {
-    btn.disabled = false;
-    prevNotificationTimeout = setTimeout(() => {
-      formNotif.innerText = ''
-    }, 3000)
-  })
+    .then((userWithId) => {
+      EDITING_USER = null;
+      form.reset();
+      switchFormToCreateUser();
+      formNotif.innerText = "Success";
+      GLOBAL_USERS = GLOBAL_USERS.map((u) => (u.id === user.id ? user : u));
+      renderUsers(GLOBAL_USERS);
+    })
+    .catch(() => {
+      formNotif.innerText = "Failed to update user";
+    })
+    .finally(() => {
+      formSubmitBtn.disabled = false;
+      prevNotificationTimeout = setTimeout(() => {
+        formNotif.innerText = "";
+      }, 3000);
+    });
+});
 
-
-})
+// init
+fetchUsers().then((users) => {
+  if (Array.isArray(users)) {
+    GLOBAL_USERS = users;
+    renderUsers(GLOBAL_USERS);
+  }
+});
--- main.py
+++ main.py
@@ -2,13 +2,17 @@ from openpyxl import Workbook, load_workbook
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from uuid import uuid4
 
 STORAGE_FILE_NAME = 'data.xlsx'
 
 class Person(BaseModel):
     firstName: str
-    lastName: str 
+    lastName: str
     age: int
+
+class PersonWithId(Person):
+    id: str
    
 #создаем экземпляр класса FastAPI который является веб сервером   
 app = FastAPI() 
@@ -51,16 +55,55 @@ app.add_middleware(
 def read_root():
     return {"Hello": "World"}
 
+# Read
 @app.post("/user")
 def create_user(user: Person):
+    id = str(uuid4())
     ws = persons_sheet
-    ws.append([user.firstName, user.lastName, user.age])
+    ws.append([id, user.firstName, user.lastName, user.age])
     wb.save(filename=STORAGE_FILE_NAME)
+    pid = PersonWithId(id=id, firstName=user.firstName, lastName=user.lastName, age=user.age)
+    return pid
 
+# Read
 @app.get("/users")
-def get_user():
+def get_users():
     persons = []
-    for row in persons_sheet.iter_rows():
-        p = Person(firstName=row[0].value, lastName=row[1].value, age=row[2].value)
-        persons.append(p)
+    for row in persons_sheet.iter_rows(values_only=True):
+        if row[0]:
+            p = PersonWithId(id=row[0], firstName=row[1], lastName=row[2], age=row[3])
+            persons.append(p)
     return persons
+
+# update
+@app.put("/user")
+def update_users(user: PersonWithId):
+    print('Updating user')
+    for row in persons_sheet.iter_rows():
+        cell = row[0]
+        if cell.value == user.id:
+            persons_sheet['B' + str(cell.row)] = user.firstName
+            persons_sheet['C' + str(cell.row)] = user.lastName
+            persons_sheet['D' + str(cell.row)] = user.age
+            wb.save(filename=STORAGE_FILE_NAME)
+            return user
+    raise Exception('User not found')
+
+# delete
+@app.delete("/user")
+def delete_user(id: str | None = None):
+    if id == None:
+        raise Exception("No ID provided")
+    
+    print('DELETE USER:' + id)
+    user = None
+    for row in persons_sheet.iter_rows():
+        cell = row[0]
+        if cell.value == id:
+            idx = cell.row
+            print('Found index of id: ' + id + ' \nindex:' + str(idx))
+            user = PersonWithId(id=row[0].value, firstName=row[1].value, lastName=row[2].value, age=row[3].value)
+            persons_sheet.delete_rows(idx)
+    return user
+    
+
